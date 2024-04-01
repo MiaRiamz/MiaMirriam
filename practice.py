dcc54f6f963edb64979147a55d2b647ff262f12c
Enock Ssemwogerere <115041750+EGOproject@users.noreply.github.com>
2024-04-01 12:02:28 +0300

    Update practice.py

--- practice.py
+++ practice.py
@@ -1,20 +1,11 @@
-
-# GROUP MEMBERS:
-# Nabiryo Miriam,
-# Ssemwogerere Enock,
-# Kaitesi Ritah,
-# Ashraf,
-# Muwayire Terry,
-
-
 class Author:
     def __init__(self, name, birthdate, nationality):
-     self.name = name
-     self.birthdate = birthdate
-     self.nationality = nationality
+        self.name = name
+        self.birthdate = birthdate
+        self.nationality = nationality
 
 class Book:
-    def __init__(self, title, author, isbn, pages,available = True):
+    def __init__(self, title, author, isbn, pages, available=True):
         self.title = title
         self.author = author
         self.isbn = isbn
@@ -22,23 +13,21 @@ class Book:
         self.available = available
 
     def __str__(self):
-        return f"{self.title} by {self.author}"
-    
+        return f"{self.title} by {self.author.name}"
+
     def get_info(self):
-        return f"{self.title} by {self.author} Isbn: {self.isbn} Pages: {self.pages} Available: {self.available}"
-    
+        return f"{self.title} by {self.author.name} Isbn: {self.isbn} Pages: {self.pages} Available: {self.available}"
 
     def checkout(self):
         if self.available:
             self.available = False
             print(f"{self.title} has been checked out.")
         else:
-            return False
-
+            print(f"{self.title} is already checked out.")
 
 class Fiction(Book):
-    def __init__(self, title, author, isbn, pages, genre,available = True):
-        super().__init__(title, author, isbn, pages,available)
+    def __init__(self, title, author, isbn, pages, genre, available=True):
+        super().__init__(title, author, isbn, pages, available)
         self.genre = genre
 
     def get_info(self):
@@ -65,7 +54,6 @@ class Library:
     def search_by_title(self, title):
         return [book for book in self.books if title.lower() in book.title.lower()]
 
-
     def search_by_author(self, author_name):
         return [book for book in self.books if author_name.lower() in book.author.name.lower()]
 
@@ -76,27 +64,23 @@ class Library:
         print("Library inventory:")
         for book in self.books:
             status = "Available" if book.available else "Checked out"
-            print(f"{book}, {status}")
-#instancing our object
-book1 = Fiction("Harry Porter","Mary Parker. T","098874564",500,"Magic")
-book2 =NonFiction("Aminata","Charles Dickens","24356577",700,"Culter")
+            print(f"{book}, {status}")
+
+# Instancing our objects
+author1 = Author("Mary Parker. T", "1990-05-15", "British")
+author2 = Author("Charles Dickens", "1812-02-07", "British")
+book1 = Fiction("Harry Potter", author1, "098874564", 500, "Magic")
+book2 = NonFiction("Aminata", author2, "24356577", 700, "Culture")
+
+# Instancing the library
 lib = Library()
-#adding books
+
+# Adding books
 lib.add_book(book1)
 lib.add_book(book2)
-#displaying all books
+
+# Displaying all books
 lib.display_inventory()
-#book1.checkout()
 
-
-
-
-
-
-
-
-
-
-
-
-
+# Checking out a book
+book1.checkout()
